8ff1eef9122ef2e6f7365b3bd7ca9f1e580773fb
Benson Li <bl4074@hotmail.com>
2024-05-23 14:15:58 -0700

    Cookies

---
+++ app/src/main/java/com/sarcasmappreciated/scryfall/CookieWebViewClient.kt
@@ -0,0 +1,13 @@
+package com.sarcasmappreciated.scryfall
+
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class CookieWebViewClient : WebViewClient() {
+    override fun onPageFinished(view: WebView?, url: String?) {
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().acceptCookie()
+        CookieManager.getInstance().flush()
+    }
+}
