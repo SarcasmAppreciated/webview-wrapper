fd3cdb9b738a78cf2646f831a035cdc77db4f4e2
Benson Li <bl4074@hotmail.com>
2024-07-31 00:44:07 -0700

    Copy

--- app/src/main/java/com/sarcasmappreciated/scryfall/MainActivity.kt
+++ app/src/main/java/com/sarcasmappreciated/scryfall/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.sarcasmappreciated.scryfall
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.URLUtil
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.sarcasmappreciated.scryfall.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private val tag = "SCRYFALL"
+    private lateinit var webView: WebView
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,20 +28,38 @@ class MainActivity : ComponentActivity() {
         binding.webview.setBackgroundColor(Color.TRANSPARENT)
         binding.webview.loadUrl(resources.getString(R.string.url))
 
-        val wV: WebView = findViewById(R.id.webview)
+        webView = findViewById(R.id.webview)
         val swipeLayout: SwipeRefreshLayout = this.findViewById(R.id.swipe)
         swipeLayout.setOnRefreshListener {
-            wV.reload()
+            webView.reload()
             swipeLayout.isRefreshing = false
+        }
+
+        webView.setOnLongClickListener {
+            copyToClipboard()
+            return@setOnLongClickListener true
         }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        val wV: WebView = findViewById(R.id.webview)
-        if (keyCode == KeyEvent.KEYCODE_BACK && wV.canGoBack()) {
-            wV.goBack()
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
             return true
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    private fun copyToClipboard() {
+        webView.hitTestResult.let{
+            val url = it.extra
+            if (URLUtil.isValidUrl(url)) {
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText(tag, url)
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(this, "Copied $url", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "There was a problem.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
